07778ef73eff86116445f8cad1deb9a916be9843
Ryan Hamblin <rhamblin.contractor@lifevantage.com>
2015-05-06 14:02:31 -0600

    Did the stuff

--- js/dataService.js
+++ js/dataService.js
@@ -1,43 +1,48 @@
 var app = angular.module('quoteBook');
 
-app.service('dataService', function () {
-  var quotes = [
-    { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
-    { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
-    { text: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
-    { text: 'Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.', author: 'Robert Frost'},
-    { text: 'The most difficult thing is the decision to act, the rest is merely tenacity.', author: 'Amelia Earhart'},
-    { text: 'Life is what happens to you while you\'re busy making other plans.', author: 'John Lennon'},
-    { text: 'What even is a jQuery?', author: 'Tyler S. McGinnis'}
-  ];
-  //create a method called getData() that returns the quotes array
-  this.getData = function(){
-    return quotes;
-  };
-  
-  //Create a method called addData() that takes in a data object verifies that data has the proper keys(text, author) then adds the obj to the end of the quotes array 
+app.service('dataService', function() {
+    var quotes = [{
+        text: 'Life isn\'t about getting and having, it\'s about giving and being.',
+        author: 'Kevin Kruse'
+    }, {
+        text: 'Whatever the mind of man can conceive and believe, it can achieve',
+        author: 'Napoleon Hill'
+    }, {
+        text: 'Strive not to be a success, but rather to be of value.',
+        author: 'Albert Einstein'
+    }, {
+        text: 'Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.',
+        author: 'Robert Frost'
+    }, {
+        text: 'The most difficult thing is the decision to act, the rest is merely tenacity.',
+        author: 'Amelia Earhart'
+    }, {
+        text: 'Life is what happens to you while you\'re busy making other plans.',
+        author: 'John Lennon'
+    }, {
+        text: 'What even is a jQuery?',
+        author: 'Tyler S. McGinnis'
+    }];
 
-   this.addData = function (textAdded, authorAdded) {
-    var quoteAdder = prompt("What is your quote?")
-    var authorAdder = prompt("What is your name?")    
-    var QuoteAdder = function (quote, author) {
-      this.text = quote;
-      this.author = author;
-    }
-      if (quoteAdder !== "" && authorAdder !== "") {
-        quotes.push(new QuoteAdder(quoteAdder, authorAdder))
-        }
+    this.getData = function() {
+        return quotes;
     };
 
-    this.removeData = function(){
-      console.log('made it to this pont');
-      var removeText = prompt('What would you like to remove?');
-      for (var i = 0; i < quotes.length; i++) {
-        if(removeText === quotes[i].text) {
-          quotes.splice(i, 1);
-          i--;
+    this.addData = function(obj) {
+        if (obj.hasOwnProperty('text') && obj.hasOwnProperty('author')) {
+            quotes.push(obj);
+
+        } else {
+            alert('Data object does not have right properties');
         }
-      };
-    }
+
+    };
+
+    this.removeData = function(quoteText) {
+        for (var i = quotes.length - 1; i >= 0; i--) {
+            if (quotes[i].text === quoteText) {
+                quotes.splice(i, 1);
+            }
+        }
+    };
 });
-  
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,13 +1,34 @@
 var app = angular.module('quoteBook');
 
-app.controller('mainCtrl', function($scope, dataService){
+app.controller('mainController', function($scope, dataService) {
 
-	$scope.quotes = dataService.getData();
-	$scope.getQuote = function(){
-		dataService.addData();
-	};
+    $scope.addName = '';
+    $scope.addQuote = '';
+    $scope.removeQuote = '';
+    $scope.sortBy = 'author';
+    $scope.order = true;
 
-	$scope.removeQuote = function(){
-		dataService.removeData();
-	};
+    var getDataFromService = function() {
+        $scope.data = dataService.getData();
+    }();
+
+    $scope.removeQuoteObj = function() {
+        dataService.removeData($scope.removeQuote);
+        $scope.removeQuote = '';
+    };
+
+
+    $scope.addQuoteObj = function() {
+        dataService.addData({
+            text: $scope.addQuote,
+            author: $scope.addName,
+        });
+        $scope.addName = '';
+        $scope.addQuote = '';
+    };
+
+
+    //dataService.removeData('Life isn\'t about getting and having, it\'s about giving and being.');
+
+
 });
